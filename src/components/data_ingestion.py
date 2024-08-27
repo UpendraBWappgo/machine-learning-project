9034c5f9b5b48484685fd16b87194ee3fc6061c7
UpendraBWappgo <upendra.bhadauria@wappgo.in>
2024-08-27 15:26:33 +0530

    completed with static files

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -50,8 +50,8 @@ class DataIngestion:
             raise CustomException(e,sys)
 
 
-if __name__ == "__main__": 
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
-    data_transformation = DataTransformation()
-    train_arr, test_arr,_ = data_transformation.initaite_data_transformation(train_data, test_data)
+# if __name__ == "__main__": 
+#     obj = DataIngestion()
+#     train_data, test_data = obj.initiate_data_ingestion()
+#     data_transformation = DataTransformation()
+#     train_arr, test_arr,_ = data_transformation.initaite_data_transformation(train_data, test_data)
